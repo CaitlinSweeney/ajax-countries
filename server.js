743667519e1e8fbc91f7a81ed334e1f696fe7625
Drew Conly <atconly@gmail.com>
2016-08-09 15:49:40 -0600

    init repo, set server.js

---
+++ server.js
@@ -0,0 +1,33 @@
+require('colors');
+
+var express    = require('express'),
+    bodyParser = require('body-parser'),
+    logger     = require('morgan'),
+    mongoose   = require('mongoose'),
+    routes     = require('./routes'),
+    path       = process.env.PORT || 1337,
+    app        = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+mongoose.connect('mongodb://localhost/ajax-countries', (error) => {
+    if(error) {
+        console.error('Oh no, could not start mongoose!', error);
+        process.exit(1); // exits a node application, anything other than 0 is considered an error
+    } else {
+        console.log('Mongoose started successfully.'.cyan);
+    }
+});
+
+app.post('*', bodyParser.json(), bodyParser.urlencoded({ extended: true }));
+
+Routes(app);
+
+app.listen(port, (error)=> {
+  if(error) {
+    console.error('The server could not start', error);
+    process.exit(1);
+  } else {
+    console.log(`Server up and running on port${port}`.yellow);
+  }
+})
